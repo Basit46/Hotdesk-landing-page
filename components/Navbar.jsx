7e2792cf45af12a81d42ec7319fd88eb89be5361
Basit46 <Hassanbasit1990@gmail.com>
2023-08-18 19:09:13 +0100

    Added changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,28 +10,27 @@ import { FaBars } from "react-icons/fa";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="relative">
-      <header className="fixed top-0 left-0 z-[999] bg-white w-screen h-[92px] px-[26.5px] flex items-center justify-between xl:justify-start xl:gap-[72px]">
-        <Link href="/">
-          <Image src="/logo.svg" width="159" height="21" alt="Hotdesk logo" />
-        </Link>
-        <nav className="flex-1 hidden xl:flex gap-[36px]">
-          <Link href="/">Membership</Link>
-          <Link href="/">Locations</Link>
-          <Link href="/">Facilities</Link>
-          <Link href="/">News</Link>
-          <Link href="/">Contact</Link>
-        </nav>
-        <div className="hidden xl:block">
-          <CustomButton text="BOOK A TOUR" />
-        </div>
-        <FaBars
-          onClick={() => setIsOpen((prev) => !prev)}
-          className="block xl:hidden text-[25px]"
-        />
-      </header>
+    <header className="z-[20] w-full h-[92px] px-[26.5px] flex items-center justify-between xl:justify-start xl:gap-[72px]">
+      <Link href="/">
+        <Image src="/logo.svg" width="159" height="21" alt="Hotdesk logo" />
+      </Link>
+      <nav className="flex-1 hidden xl:flex gap-[36px]">
+        <Link href="/">Membership</Link>
+        <Link href="/">Locations</Link>
+        <Link href="/">Facilities</Link>
+        <Link href="/">News</Link>
+        <Link href="/">Contact</Link>
+      </nav>
+      <div className="hidden xl:block">
+        <CustomButton text="BOOK A TOUR" />
+      </div>
+      <FaBars
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="block xl:hidden text-[25px]"
+      />
+
       <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
-    </div>
+    </header>
   );
 };
 
